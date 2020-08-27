d65443f868da20eff729dd1330df7a08de8343f3
Batjargal Bayarsaikhan <b.batja@gmail.com>
2020-08-27 03:09:55 -0400

    Updates

---
+++ precache-manifest.ad484dfec23a7f596101ea5d52a6ef73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6242eea02d25acc5aa7bae7d0bfb6a91",
+    "url": "/histogram-chart/index.html"
+  },
+  {
+    "revision": "d60f94c4127220ce2ed1",
+    "url": "/histogram-chart/static/css/main.f01b686f.chunk.css"
+  },
+  {
+    "revision": "b408fa2f42ba3ef389b9",
+    "url": "/histogram-chart/static/js/2.6cac2c9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/histogram-chart/static/js/2.6cac2c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60f94c4127220ce2ed1",
+    "url": "/histogram-chart/static/js/main.b84e83b4.chunk.js"
+  },
+  {
+    "revision": "346dcd947480a7d806c7",
+    "url": "/histogram-chart/static/js/runtime-main.332f21b9.js"
+  }
+]);
